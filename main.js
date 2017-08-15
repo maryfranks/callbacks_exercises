ee218f3e40e1f233729b283c60e50e6eea5e6f9f
maryfranks <maryfranks@gmail.com>
2017-08-14 20:32:47 -0400

    return number of credit purchases

--- main.js
+++ main.js
@@ -202,7 +202,12 @@ console.log( 'The total number of cash sales is:', numCashSales );
   HINT(S):
   - Make sure to exclude any 'sales' made by 'credit'!
 */
-var numCreditPurchases;
+
+function creditPurchases(transaction) {
+  return (transaction['type'] === 'purchase' && transaction['paymentMethod'] === 'credit');
+}
+
+var numCreditPurchases = transactions.filter(creditPurchases).length;
 
 console.log( 'The total number of credit purchases is:', numCreditPurchases );
 
